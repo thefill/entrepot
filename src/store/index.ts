2c9a4cbeaf00f1c9bc51cdd9cb474d2708f83ff9
Filip Dabrowski <fifofil@gmail.com>
2018-07-24 10:31:37 +0100

    Cleanup

--- src/store/index.ts
+++ src/store/index.ts
@@ -12,17 +12,30 @@ export interface IStoreEntryConfig {
 }
 
 export interface IStoreEntry {
-    value: any;
+    currentStep: number;
+    history: any[]
 }
 
 export class Store {
+
+    // TODO: get history list
+    // TODO: get history length
+    // TODO: get history list with values
+    // TODO: move back in time x steps
+    // TODO: get value from x step
+    // TODO: move back in time to prev
+    // TODO: remove
+    // TODO: reset
+    // TODO: move back in time to next (if keep forward)
+    // TODO: emit change
+    // TODO: automatic documentation via http://typedoc.org/guides/doccomments/
 
     private static decoupleValue(value: any): any {
         if (!value) {
             return value;
         }
 
-        if(Array.isArray(value)) {
+        if (Array.isArray(value)) {
             return value.slice();
         } else if (typeof value === 'object') {
             return Object.assign({}, value);
@@ -31,45 +44,59 @@ export class Store {
         return value;
     }
 
-    protected store: {[namespace: string]:IStoreEntry | {[key: string]:IStoreEntry}} = {};
+    protected store: { [namespace: string]: IStoreEntry | { [key: string]: IStoreEntry } } = {};
 
     public set(key: string | IStoreEntryConfig, value?: any): StoreEntryKey | void {
 
-        key = new StoreEntryKey(key);
+        const keyObject = new StoreEntryKey(key);
 
-        if (!key.key) {
+        if (!keyObject.key) {
             return;
         }
 
         value = Store.decoupleValue(value);
 
-        if(key.namespace){
+        this.setOrUpdate(keyObject, value);
+
+        return this.setOrUpdate(keyObject, value) ? keyObject;
+    }
+
+    public setOrUpdate(key: StoreEntryKey, value: any): boolean {
+        if (!this.entryExists(key)) {
+            return false;
+        }
+
+        if (key.namespace) {
             this.store[key.namespace] = {[key.key]: value};
         } else {
             this.store[key.key] = value;
         }
 
-        // TODO: emit change
+        return true;
     }
 
-    // TODO: get history list
-    // TODO: get history length
-    // TODO: get history list with values
-    // TODO: move back in time x steps
-    // TODO: get value from x step
-    // TODO: move back in time to prev
-    // TODO: move back in time to next (if keep forward)
+    public entryExists(key: StoreEntryKey): boolean {
+        if (key.namespace) {
+            return !!(this.store[key.namespace] && this.store[key.key]);
+        } else {
+            return !!this.store[key.key];
+        }
+    }
 
-
+    /**
+     * G
+     * @param {string | IStoreEntryConfig | StoreEntryKey} key
+     * @returns {void | T}
+     */
     public get<T>(key: string | IStoreEntryConfig | StoreEntryKey): T | void {
         if (!key) {
-            return ;
+            return;
         }
 
         let keyValue;
         let namespaceValue;
 
-        if(typeof key === 'string'){
+        if (typeof key === 'string') {
             keyValue = key;
         } else {
             keyValue = key.key;
--- src/store/store-entry-key.ts
+++ src/store/store-entry-key.ts
@@ -1,24 +1,32 @@
 import {IStoreEntryConfig} from "./index";
 
+/**
+ * Represents key for the store entry
+ */
 export class StoreEntryKey {
+    // Entry namespace key
     public namespace: string;
+    // Entry key
     public key: string;
 
-    constructor(key: string | IStoreEntryConfig){
+    constructor(key: string | IStoreEntryConfig | StoreEntryKey) {
 
-        if(typeof key === 'string'){
+        // if key object passed - don't create again
+        if (key instanceof StoreEntryKey) {
+            Object.assign(this, key);
+            return;
+        }
+
+        // if not key config
+        if (typeof key === 'string') {
             this.key = key;
             return;
-        } else if(key.key){
+        } else if (key.key) {
             this.key = key.key;
-        }
 
-        if (!this.key) {
-            return;
-        }
-
-        if (key.namespace){
-            this.namespace = key.namespace;
+            if (key.namespace) {
+                this.namespace = key.namespace;
+            }
         }
     }
 }
